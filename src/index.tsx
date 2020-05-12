37015406dc1a61d5176497e70a084788576215bd
no <no>
2020-05-12 17:04:59 +0300

    fix

---
+++ src/index.tsx
@@ -0,0 +1,34 @@
+import { renderToString } from 'react-dom/server';
+import { ServerStyleSheets } from '@material-ui/core/styles';
+import Head from 'next/head';
+import React, { FC } from 'react';
+
+const isServer = () => process && !(process as any).browser;
+
+const SSRProvider: FC = ({ children }) => {
+    if (isServer()) {
+        const sheets = new ServerStyleSheets();
+        const output = sheets.collect(children);
+        const html = renderToString(output);
+        return (
+            <>
+                <Head>{sheets.getStyleElement()}</Head>
+                <div
+                    id="ssr-content"
+                    dangerouslySetInnerHTML={{ __html: html }}
+                />
+            </>
+        );
+    } else {
+        return (
+            <>
+                <Head>
+                    <style id="jss-server-side"></style>
+                </Head>
+                <div id="ssr-content">{children}</div>
+            </>
+        );
+    }
+};
+
+export default SSRProvider;
